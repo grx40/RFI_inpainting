fb732c877f135ef0fda87575b2525ef31260ab86
Michael <michael.pagano@mail.mcgill.ca>
2021-05-26 19:01:09 -0400

    custom callbacks

--- callbacks.py
+++ callbacks.py
@@ -7,3 +7,7 @@ class CustomCallbacks(keras.callbacks.Callback):
     def on_train_begin(self, logs=None):
         keys = list(logs.keys())
         print("Starting training; got log keys: {}".format(keys))
+
+    def on_epoch_end(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print("End epoch {} of training; got log keys: {}".format(epoch, keys))
